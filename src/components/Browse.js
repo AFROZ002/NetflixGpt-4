3d4119cd18ae6945e34760e7bd2feaafa5abf136
Afroz Alam <afrozcpr@gmail.com>
2024-08-19 22:34:35 +0530

    First day of Project

---
+++ src/components/Browse.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomimgMovies from "../hooks/useUpcomimgMovies";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleToGptPage } from "../utils/slices/gptSlice";
+import { addPath } from "../utils/slices/pathSlice";
+import Shimmer from "./Shimmer";
+import ShowcaseShimmer from "./ShowcaseShimmer";
+
+const Browse = () => {
+  const user = useSelector((store) => store.user);
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
+
+  useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomimgMovies();
+  const dispatch = useDispatch();
+
+  const pathname = window.location.pathname;
+
+  useEffect(() => {
+    dispatch(toggleToGptPage());
+    dispatch(addPath(pathname));
+  }, []);
+
+  return nowPlayingMovies === null || user === null ? (
+    <Shimmer /> //Shimmer
+  ) : (
+    <div className="w-full h-full">
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
+  );
+};
+
+export default Browse;
